ce0a84daa196248e8986f41329daebc539840a30
natalka1122 <natalka1122@yandex.ru>
2020-11-04 18:07:29 +0300

    add defaults to env variables

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
 if TELEGRAM_TOKEN is None:
     print(
         "There is no telegram token. Please provide TELEGRAM_TOKEN in .env file or as environment variable",
@@ -17,7 +17,7 @@ if TELEGRAM_TOKEN is None:
     )
     sys.exit(1)
 
-GLPI_BASE_URL = os.getenv("GLPI_BASE_URL")
+GLPI_BASE_URL: str = os.getenv("GLPI_BASE_URL")
 if GLPI_BASE_URL is None:
     print(
         "There is no GPLI API url. Please provide GLPI_BASE_URL in .env file or as environment variable",
@@ -25,26 +25,24 @@ if GLPI_BASE_URL is None:
     )
     sys.exit(1)
 
-DATA_DIR = os.getenv("DATA_DIR")
-if DATA_DIR is None:
-    DATA_DIR = "data/"
-os.makedirs(DATA_DIR, exist_ok=True)
-DB_FILE = DATA_DIR + "db.db"
-STATE_FILE = DATA_DIR + "state.json"
-LOG_FILENAME = DATA_DIR + "log.txt"
+_data_dir: str = os.getenv("DATA_DIR", default="data/")
+os.makedirs(_data_dir, exist_ok=True)
+DB_FILE: str = _data_dir + "db.db"
+STATE_FILE: str = _data_dir + "state.json"
+LOG_FILENAME: str = _data_dir + "log.txt"
 
-log_level = os.getenv("LOG_LEVEL").upper()
-if log_level == "CRITICAL":
-    LOG_LEVEL = logging.CRITICAL
-elif log_level == "ERROR":
-    LOG_LEVEL = logging.ERROR
-elif log_level == "WARNING":
-    LOG_LEVEL = logging.WARNING
-elif log_level == "INFO":
-    LOG_LEVEL = logging.INFO
-elif log_level == "DEBUG":
-    LOG_LEVEL = logging.DEBUG
-elif log_level == "NOTSET":
-    LOG_LEVEL = logging.NOTSET
+_log_level: str = os.getenv("LOG_LEVEL", default="").upper()
+if _log_level == "CRITICAL":
+    LOG_LEVEL: str = logging.CRITICAL
+elif _log_level == "ERROR":
+    LOG_LEVEL: str = logging.ERROR
+elif _log_level == "WARNING":
+    LOG_LEVEL: str = logging.WARNING
+elif _log_level == "INFO":
+    LOG_LEVEL: str = logging.INFO
+elif _log_level == "DEBUG":
+    LOG_LEVEL: str = logging.DEBUG
+elif _log_level == "NOTSET":
+    LOG_LEVEL: str = logging.NOTSET
 else:
-    LOG_LEVEL = logging.INFO
+    LOG_LEVEL: str = logging.INFO
