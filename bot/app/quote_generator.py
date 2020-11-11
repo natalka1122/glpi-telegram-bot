2634a8378073b67e41cf901c004b699676a9d15c
natalka1122 <natalka1122@yandex.ru>
2020-11-11 11:42:55 +0300

    fix docstring

--- bot/app/quote_generator.py
+++ bot/app/quote_generator.py
@@ -1,20 +1,22 @@
 """Genetate random quote
 """
 import json
+import typing
 import requests
 
 
-def get_quote():
-    """Generate
+def get_quote() -> str:
+    """Get random quote from https://forismatic.com/ru/api/
 
     Returns:
-        [type]: [description]
+        str: [description]
     """
-    url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru"
-    response = json.loads(requests.request("GET", url, headers={}, data={}).text)
-    print(type(response), response)
-    quote_author = response["quoteAuthor"]
-    quote_text = response["quoteText"]
+    url: str = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru"
+    response: typing.Dict = json.loads(
+        requests.request("GET", url, headers={}, data={}).text
+    )
+    quote_author: str = response["quoteAuthor"]
+    quote_text: str = response["quoteText"]
     if len(quote_author) > 0:
         return f"{quote_text} (c) {quote_author}"
     return quote_text
